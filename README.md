e89e8aa65ff446016a3ed31d99bfeab63b2e505b
Albert Fabrizi <alfabrizi3@gmail.com>
2024-11-14 14:41:49 -0500

    added run range capability

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Description
 This is to match ROOT Trees outputted from the ReactionFilter to PSFluxTree files then calculate the total flux from the those files.
 
-Test
+# Quick Run Guide
+First find the top directory that contains all the files from the ReactionFilter and the tree_PSFlux. In the settings.config paste the directory for the output from the reaction filter in the INPUT_TOPDIR option. Then paste the directory for the tree_PSFlux in the FLUX_DIR option (you probably won't need to change this unless you have custom PSFlux trees. Then type the range in below starting with the first run number and then the last run number.
+
+Then to run: python get_flux_vals.py
+
+# DO NOT MIX EMPTY AND FULL RUNS
--- get_flux_vals.py
+++ get_flux_vals.py
@@ -1,12 +1,15 @@
+import configparser
+import argparse
 import os
 import re
 import ROOT
 
-#source_dir = '/volatile/halld/home/alfab/CPP_REACTIONFILTER/fulltarget/pippim/tree_gpb208_pippimmisspb208__B4/101582/'
-#flux_dir = '/cache/halld/RunPeriod-2022-05/recon/ver01/tree_PSFlux/101582/'
+#input_topdir = '/volatile/halld/home/alfab/CPP_REACTIONFILTER/fulltarget/pippim/tree_gpb208_pippimmisspb208__B4/'
+#flux_dir = '/cache/halld/RunPeriod-2022-05/recon/ver01/tree_PSFlux/'
 
 def get_file_numbers(directory):
     files = os.listdir(directory)
+    
     pattern = re.compile(r'.*_(\d{3})\.root$')
     numbers = set()
     for filename in files:
@@ -15,10 +18,10 @@ def get_file_numbers(directory):
             numbers.add(match.group(1))
     return sorted(numbers)
 
-def find_matching_files(flux_dir, numbers):
+def find_matching_files(flux_dir,run_number,numbers):
     matching_files = []
     for number in numbers:
-        filename = f"tree_PSFlux_101604_{number}.root"
+        filename = f"{run_number}/tree_PSFlux_{run_number}_{number}.root"
         file_path = os.path.join(flux_dir, filename)
         if os.path.exists(file_path):
             matching_files.append(file_path)
@@ -54,27 +57,41 @@ def integrate_over_branch(files, branch_name, lower_bound, upper_bound):
 
     return total_integral
 
-def process_files(source_dir, flux_dir, branch_name, lower_bound, upper_bound):
+def process_files(input_dir, flux_dir, branch_name, lower_bound, upper_bound,start_run,end_run):
 
-    numbers = get_file_numbers(source_dir)
-    
-
-    matching_files = find_matching_files(flux_dir, numbers)
-    
-    if not matching_files:
-        print("No matching files found.")
-        return
-    
-
-    total_integral = integrate_over_branch(matching_files, branch_name, lower_bound, upper_bound)
-    print(f"Total integral over branch '{branch_name}': {total_integral}")
+    i = start_run
+    total_integral_over_runs = 0
+    while i <= end_run:
+        source_dir = input_dir + str(i) + '/'
+        numbers = get_file_numbers(source_dir)
+        matching_files = find_matching_files(flux_dir, i ,numbers)
+        if not matching_files:
+            print("No matching files found.")
+            return
+        total_integral = integrate_over_branch(matching_files, branch_name, lower_bound, upper_bound)
+        print(f"Total integral over PSFluxEnergy for run: '{i}': {total_integral}")
+        total_integral_over_runs += total_integral
+        i += 1
+    print(f"Total integral over PSFluxEnergy for all selected runs: {total_integral_over_runs}")
 if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read('settings.config')
+
+    input_topdir = config['Directories']['INPUT_TOPDIR']
+    flux_topdir = config['Directories']['FLUX_DIR']
+    
+    start_run = config['OtherSettings'].getint('START_RUN')
+    end_run = config['OtherSettings'].getint('END_RUN')
+
+
+    
     process_files(
-        #source_dir = '/volatile/halld/home/alfab/CPP_REACTIONFILTER/fulltarget/pippim/tree_gpb208_pippimmisspb208__B4/101582/',
-        source_dir = '/volatile/halld/home/alfab/CPP_REACTIONFILTER/mttarget/pippim/tree_gpb208_pippimmisspb208__B4/101604/',
-        #flux_dir = '/cache/halld/RunPeriod-2022-05/recon/ver01/tree_PSFlux/101582/',
-        flux_dir = '/cache/halld/RunPeriod-2022-05/recon/ver01/tree_PSFlux/101604/',
+        input_dir = input_topdir,
+        flux_dir = flux_topdir,
         branch_name='PSPairEnergy', 
-        lower_bound=4.0,
-        upper_bound=6.0 
+        lower_bound=0.0,
+        upper_bound=12.0,
+        start_run = start_run,
+        end_run = end_run
     )
